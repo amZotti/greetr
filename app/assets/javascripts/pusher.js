f4797df93d3516d7d8b7aadbffddc5ab05b17dad
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-11 14:47:37 -0400

    Fix typos in pusher.js and layout. Fix params in greetings controller

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -1,29 +1,20 @@
 $(function(){
 
-  Pusher.log = function(message) {
-    if (window.console && window.console.log) {
-      window.console.log(message);
-    }
-  };
+  $("#new_greeting").submit(function(){
+    $.post(
+      "/greetings",
+      $("#new_greeting").serialize())
+      return false;
+  });
 
-  var pusher = new Pusher('4824e30a70e22f04260e');
+  var pusher = new Pusher(window.PUSHER);
   var channel = pusher.subscribe(window.CHANNEL);
+
   channel.bind('new_greeting', function(data) {
-    alert(data.message);
+    $(".greetings").prepend(data.greeting);
+
   });
 
 
 });
 
-//tune into server
-//var pusher = new Pusher('pusher id');
-
-//tune into channel on server
-//var channel = pusher.subscribe('test_channel');
-
-//listen to a specific event on the channel on the server and execute callback when received
-//channel.bind('my_event', function(data) {
-//  alert(data.message)
-//});
-//
-//:wq
--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,18 +1,14 @@
 class GreetingsController < ApplicationController
   def create
-    current_user.sent_greetings.create(greetings_params)
-    Pusher[params[:recipient_id].to_s].trigger('new_greeting', {
-      message: message_params, email: current_user.email
+    greeting = current_user.sent_greetings.create(greetings_params)
+    html_greeting = render greeting
+    Pusher[greetings_params[:recipient_id].to_s].trigger('new_greeting', {
+      greeting: html_greeting
     })
-    redirect_to :dashboard
   end
 
   private
   def greetings_params
-    params.require(:greeting).permit(:message,:recipient_id)
-  end
-  
-  def message_params
-    params.require(:greeting).permit(:message)
+    params.require(:greeting).permit(:message, :recipient_id)
   end
 end
